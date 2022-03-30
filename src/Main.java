380ec064bb5279c78ddd3c4ef0a0e5499887a6f6
Berke <berkeuslu0@gmail.com>
2022-03-30 22:31:34 +0200

    -code cleanup

--- src/Main.java
+++ src/Main.java
@@ -6,29 +6,23 @@ public class Main {
             //if(a == 0 || b == 0) {
             //    return 1;
             //}
-             while (b != 0) {
+            while (b != 0) {
                 int temp = b;
                 b = a % b;
                 a = temp;
             }
             return a;
         }
+            public static void main (String[] args){
+                Scanner scanner = new Scanner(System.in);
+                System.out.print("Please enter the A value:");
+                int a = scanner.nextInt();
+                System.out.print("Please enter the B value:");
+                int b = scanner.nextInt();
+                scanner.close();
+                int result = findGCD(a, b);
+                System.out.println("GCD of (" + a + " , " + b + ") : " + result);
+            }
 
-        public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("Please enter the A value:");
-            int a = scanner.nextInt();
-            System.out.println("");
-
-            System.out.print("Please enter the B value:");
-            int b = scanner.nextInt();
-            System.out.println();
-
-            scanner.close();
-            int result = findGCD(a, b);
-
-            System.out.println("GCD(" + a + " , " + b + ") = " + result);
         }
-
     }
-}
